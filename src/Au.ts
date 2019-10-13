c084cd47a30d5af906c5714a77c3fa2c6148b945
Pikaoku <pikaoku@gmail.com>
2019-10-13 13:32:08 +0100

    Feat: add remaining core instructions

--- src/Au.ts
+++ src/Au.ts
@@ -11,4 +11,23 @@ export default class Au {
 	public static move(a: array, from: int, to: int): array {
 		return Au.addAt(Au.remove(a, from), to, a[from])
 	}
+	public static remove(a: array, index: int): array {
+		return a.filter((value, i) => i !== index)
+	}
+
+	public static replace(a: array, index: int, value: any): array {
+		return a.map((v, i) => (i === index ? value : v))
+	}
+
+	public static add(a: array, value: any): array {
+		return [...a, value]
+	}
+
+	public static addManyAt(a: array, index: int, values: array) {
+		return [...a.slice(0, index), ...values, ...a.slice(index)]
+	}
+
+	public static addAt(a: array, index: int, value: any) {
+		return Au.addManyAt(a, index, [value])
+	}
 }
--- src/__tests__/Au.test.ts
+++ src/__tests__/Au.test.ts
@@ -9,6 +9,35 @@ test('Clone', () => {
 	expect(Au.clone(ar)).not.toBe(ar)
 })
 
+test('Remove', () => {
+	const ar: any[] = ['a', 'b', 'c']
+	expect(Au.remove(ar, 1)).toEqual(['a', 'c'])
+	expect(ar).toEqual(['a', 'b', 'c'])
+})
+
+test('Add', () => {
+	const ar: any[] = []
+	expect(Au.add(ar, 'a')).toEqual(['a'])
+	expect(Au.add(ar, 'b')).toEqual(['b'])
+	expect(Au.add(Au.add(ar, 'a'), 'b')).toEqual(['a', 'b'])
+	expect(ar).toEqual([])
+})
+
+test('Add At', () => {
+	const ar: any[] = ['a', 'b', 'c']
+	expect(Au.addAt(ar, 1, 'aa')).toEqual(['a', 'aa', 'b', 'c'])
+	expect(ar).toEqual(['a', 'b', 'c'])
+})
+
+test('Add Many At', () => {
+	const ar: any[] = ['a', 'b']
+	expect(Au.addManyAt(ar, 1000, ['c', 'd'])).toEqual(['a', 'b', 'c', 'd'])
+	expect(Au.addManyAt(ar, -2, ['c', 'd'])).toEqual(['c', 'd', 'a', 'b'])
+	expect(Au.addManyAt(ar, 2, ['c', 'd'])).toEqual(['a', 'b', 'c', 'd'])
+	expect(Au.addManyAt(ar, 0, ['c', 'd'])).toEqual(['c', 'd', 'a', 'b'])
+	expect(ar).toEqual(['a', 'b'])
+})
+
 test('Move', () => {
 	const ar: any[] = ['a', 'b', 'c']
 	expect(Au.move(ar, 0, 1)).toEqual(['b', 'a', 'c'])
@@ -16,4 +45,12 @@ test('Move', () => {
 	expect(Au.move(ar, 0, 400)).toEqual(['b', 'c', 'a'])
 	expect(ar).toEqual(['a', 'b', 'c'])
 })
+
+test('Replace', () => {
+	const ar: any[] = ['a', 'b', 'c']
+	expect(Au.replace(ar, 1, 'v')).toEqual(['a', 'v', 'c'])
+	expect(Au.replace(ar, 400, 'v')).toEqual(['a', 'b', 'c'])
+	expect(Au.replace(ar, -1, 'v')).toEqual(['a', 'b', 'c'])
+	expect(Au.replace(ar, 2, 'v')).toEqual(['a', 'b', 'v'])
+	expect(ar).toEqual(['a', 'b', 'c'])
 })
