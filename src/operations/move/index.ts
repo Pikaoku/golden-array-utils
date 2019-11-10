67e39a313b038fd9c3c4c2da989df56f9e45332e
Pikaoku <pikaoku@gmail.com>
2019-11-10 18:27:18 +0000

    Feat (Feat): move

--- src/operations/move/index.ts
+++ src/operations/move/index.ts
@@ -1,1 +1,6 @@
-export default () => ({})
+import { integer } from '../../types'
+import addAt from '../addAt'
+import remove from '../remove'
+
+export default (array: any[], from: integer, to: integer) =>
+	addAt(remove(array, from), to, array[from])
--- src/operations/move/move.test.ts
+++ src/operations/move/move.test.ts
@@ -1,3 +1,5 @@
-test('Placeholder', () => {
-	expect(true).toBeTruthy()
+import move from '.'
+
+test('simple move', () => {
+	expect(move([1, 2], 1, 0)).toEqual([2, 1])
 })
