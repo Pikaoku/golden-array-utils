6b3094da1583586afbc0c8c5f70cd75821ecd582
Pikaoku <pikaoku@gmail.com>
2019-11-11 19:38:33 +0000

    Fix: remove test that didn't add anything

--- src/operations/addAt/addAt.test.ts
+++ src/operations/addAt/addAt.test.ts
@@ -7,11 +7,3 @@ test('original values retained in array', () => {
 test('new value returned in array', () => {
 	expect(addAt([1], 0, 2)).toContain(2)
 })
-
-test('index must be an integer', () => {
-	expect(() => addAt([1], 4.2, 2)).toThrowError(TypeError)
-})
-
-test('index must be a positive integer', () => {
-	expect(() => addAt([1], -1, 2)).toThrowError(TypeError)
-})
